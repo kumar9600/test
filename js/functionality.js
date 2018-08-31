f09e8c3537151c1b1f879b5e0b08fd05e73651cd
kumar9600 <vazhavanthakumar@gmail.com>
2018-08-31 23:34:59 +0530

    project initialized

---
+++ js/functionality.js
@@ -0,0 +1,10 @@
+// var login = document.getElementById("loginBtn");
+// if (login) {
+  loginBtn.addEventListener("click", redirect);
+
+// }
+
+function redirect() {
+  alert(hai);
+  window.location = "D:\WebProjects\MyFirstWebPage\dashboard.html";
+}
